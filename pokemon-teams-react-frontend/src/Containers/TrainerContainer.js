a6110211bb7de716479902cc0505cddf8ab3788f
Princeton Rose <princetonjrose@gmail.com>
2020-12-16 00:56:51 -0500

    got backend setup finished

---
+++ pokemon-teams-react-frontend/src/Containers/TrainerContainer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+const TrainerContainer =( )=> {
+
+    return (
+        <div>
+            <main id='main'>
+
+            </main>
+        </div>
+    )
+}
+
+export default TrainerContainer
